e044d45803267dd7565c896c16a1dc6bbacdad49
Edward Jobs <qchpeng@hitotek.com>
2019-10-19 13:49:39 +0800

    event://add fields

--- packages/core/src/event/Event.ts
+++ packages/core/src/event/Event.ts
@@ -1,11 +1,32 @@
+import Position from '../view/Position'
+
 class Event {
     private readonly _trigger: string
-
+    private _position: Position
+    private _button: number
 
     constructor (trigger: string) {
         this._trigger = trigger
+        this._position = new Position()
+        this._button = -1
     }
 
+
+    get button (): number {
+        return this._button
+    }
+
+    set button (value: number) {
+        this._button = value
+    }
+
+    get position (): Position {
+        return this._position
+    }
+
+    set position (value: Position) {
+        this._position = value
+    }
 
     get trigger (): string {
         return this._trigger
